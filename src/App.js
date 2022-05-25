242b26b5a44cb0a9395a015681c442032f102883
Gudge777 <gudge777@mail.ru>
2022-05-25 18:04:02 +0300

    promise in return

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useFetch} from "react-async";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getUsers () {
+    const url = 'https://reqres.in/api/users?page=2';
+    let resolve = await fetch(url);
+    let json = await resolve.json();
+    let users = json.data; //это нужные данные
+    console.log (users);
+    return(users);
+}
+
+const App = () => {
+    let users = getUsers(); //это ебаный промис
+
+    //let users = usersPromise.json();
+    let usersComponent = users.map(user => <p key={user.id}>user.last_name</p>);
+    return (
+        <div className="App">
+            {usersComponent}
+            абракадабра
+        </div>
+    );
 }
 
 export default App;
